131abfc8c302d074f16806bb7ed50dbe2b2a2af3
filipvujic <filip.vujic665@gmail.com>
2022-11-23 14:35:48 +0100

    Added init project files

---
+++ mvpdemo/src/main/java/com/filipv/mvpdemo/MvpdemoApplication.java
@@ -0,0 +1,13 @@
+package com.filipv.mvpdemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MvpdemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MvpdemoApplication.class, args);
+	}
+
+}
---
+++ mvpdemo/src/test/java/com/filipv/mvpdemo/MvpdemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.filipv.mvpdemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MvpdemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
